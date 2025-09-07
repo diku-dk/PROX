514b55e4437644ff10df85bd897c0bc8aef90bc3
Rasmus G. Nielsen <ncg106@alumni.ku.dk>
2025-09-07 12:41:54 +0200

    More conversions and fixes

--- PROX/FOUNDATION/GEOMETRY/GEOMETRY/include/types/geometry_ray.h
+++ PROX/FOUNDATION/GEOMETRY/GEOMETRY/include/types/geometry_ray.h
@@ -2,68 +2,6 @@
 #define GEOMETRY_RAY_H
 
 #include "tiny_math_types.h"
-namespace geometry
-{
-
-  template<typename V>
-  class Ray
-  {
-  public:
-      using T = typename V::real_type;
-      using VT = typename V::value_traits;
-
-  protected:
-      V m_origin;
-      V m_direction;
-
-  public:
-
-    V const & origin()    const { return this->m_origin; }
-    V       & origin()          { return this->m_origin; }
-
-    V const & direction() const { return this->m_direction; }
-
-    void set_direction(V const & direction)
-    {
-      this->m_direction = unit(direction);
-    }
-
-  public:
-
-    Ray()
-    : m_origin( V::zero() )
-    , m_direction(  V::k() )
-    {}
-
-    Ray(V const & origin, V const & direction)
-    : m_origin(origin)
-    , m_direction( unit( direction ) )
-    {}
-
-    Ray(Ray const & ray)
-    : m_origin(ray.m_origin)
-    , m_direction(ray.m_direction)
-    {}
-
-    Ray & operator=(Ray const & ray)
-    {
-      if(this!=&ray)
-      {
-        this->m_origin    = ray.m_origin;
-        this->m_direction = ray.m_direction;
-      }
-      return *this;
-    }
-
-  };
-
-  template<typename V>
-  inline Ray<V> make_ray(V const & origin, V const & direction)
-  {
-    return Ray<V>(origin, direction);
-  }
-
-}// namespace geometry
 
   namespace geometry
   {
@@ -122,14 +60,6 @@ namespace geometry
       return RayEigen<T>(origin, direction);
   }
 
-  template<typename V>
-  RayEigen<typename V::real_type> convertRayToEigen(const Ray<V> ray)
-  {
-      RayEigen<typename V::real_type> newRay;
-      newRay.set_direction(toEigen(ray.direction()));
-      newRay.origin() = toEigen(ray.origin());
-      return newRay;
-  }
   }// namespace geometry
 
 
